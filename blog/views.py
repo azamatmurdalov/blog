e6a45bd08aa5328f30238d2cb8a97fd91642d76e
azamatmurdalov <azamat.murdalov@mail.ru>
2018-11-17 00:39:34 +0300

    I added 'home' and 'about' templates. I also added data from the dictionary to templates via views

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+	{
+		'author': 'Азамат Мурдалов',
+		'title': 'Первый пост',
+		'content': 'Контент первого поста',
+		'date_posted': 'Август 27, 2018'
+	},
+	{
+		'author': 'Ислам Баталов',
+		'title': 'Второй пост',
+		'content': 'Контент второго поста',
+		'date_posted': 'Август 28, 2018'
+	}
+]
 
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html', {'title': 'Обо мне'})
